a860102a9e3ce1d279d6f673b4cd4fd489b40fed
andresffp93 <andresffp93@gmail.com>
2020-04-03 22:01:04 -0500

    Nuevo comentario en el void main

--- Prueba_repositorio/Prueba_repositorio/Program.cs
+++ Prueba_repositorio/Prueba_repositorio/Program.cs
@@ -17,7 +17,8 @@ namespace Prueba_repositorio
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // Holaa
+            // Hola
+            // Nuevo comentario
         }
     }
 }
